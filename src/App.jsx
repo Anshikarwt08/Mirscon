3f860b7a3b6d55398bbfdfaa8858499d17a23d9e
kriti <bartwalk30@gmail.com>
2026-07-20 11:27:53 +0530

    created testimonial section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Home1 from './pages/Home/Home1/Home1';
 import Home2 from './pages/Home/Home2/Home2';
+import Testimonial from './pages/Testimonials/Testimonial';
 function App() {
 
   return (
     <>
     <Home1 />
     <Home2/>
+    <Testimonial/>
     </>
   )
 }
